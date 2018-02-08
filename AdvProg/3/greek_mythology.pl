3248ac9daca2050e1b1c202032918fd5d046e34b
Ryuya Ikeda <rikeda71@gmail.com>
2018-02-08 16:56:59 +0900

    question 3 of AdvProg

---
+++ AdvProg/3/greek_mythology.pl
@@ -0,0 +1,26 @@
+grandchild(X, Z) :- child(X, Y), child(Y, Z).
+
+% クロノスとレアの子供
+child(Hestia, Kronos).
+child(Demeter, Kronos).
+child(Hera, Kronos).
+child(Hades, Kronos).
+child(Poseidon, Kronos).
+child(Zeus, Kronos).
+
+% ゼウスの子供
+child(Athena, Zeus).
+child(Apollon, Zeus).
+child(Artemis, Zeus).
+child(Ares, Zeus).
+child(Hephaistos, Zeus).
+child(Dionysus, Zeus).
+child(Perseus, Zeus).
+child(Hercules, Zeus).
+child(Minos, Zeus).
+child(Persephone, Zeus).
+child(Helene, Zeus).
+child(Aglaia, Zeus).
+child(Euphrosyne, Zeus).
+child(Thalia, Zeus).
+
